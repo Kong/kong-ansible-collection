af8efaade44c2ab9f60a9233a347b7e461c578a0
Andrew J. Huffman <andrew.huffman@konghq.com>
2022-10-04 17:12:27 -0400

    refactor: drop unnecessary imports
    Signed-off-by: Andrew J. Huffman <andrew.huffman@konghq.com>

--- plugins/module_utils/inso_args_common.py
+++ plugins/module_utils/inso_args_common.py
@@ -1,19 +1,37 @@
-
-from __future__ import absolute_import, division, print_function
-
-from ansible.module_utils.six import string_types
-
-__metaclass__ = type
-
-# may not need func
-def list_dict_str(value):
-    if isinstance(value, (list, dict, string_types)):
-        return value
-    raise TypeError
-
 COMMON_ARG_SPEC = {
-    "binary_path": {"type": "path", "default": "inso"},
+    "binary_path": {"type": "path", "required": False},
     "config": {"type": "path", "required": False},
     "working_dir": {"type": "path", "required": False},
     "src": {"type": "path", "required": False}
 }
+
+def build_base_cmd(module):
+    cmd = []
+    # get inso binary path
+    if not module.params["binary_path"]:
+        cmd.append(module.get_bin_path(
+            "inso",
+            required=True)
+        )
+    else:
+        cmd.append(module.get_bin_path(
+            module.params["binary_path"],
+            required=True)
+        )
+
+    # --config
+    if module.params["config"]:
+        cmd.append("--config")
+        cmd.append(module.params["config"])
+
+    # --src
+    if module.params["src"]:
+        cmd.append("--src")
+        cmd.append(module.params["src"])
+
+    # --workingDir
+    if module.params["working_dir"]:
+        cmd.append("--workingDir")
+        cmd.append(module.params["working_dir"])
+
+    return cmd
